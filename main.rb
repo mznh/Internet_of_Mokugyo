d4384ae0a3f94f603ebeaf476b86b287f9b42e7d
mznh <mznh.jp@gmail.com>
2018-07-04 21:53:00 +0900

    tab周りを修正

--- main.rb
+++ main.rb
@@ -8,29 +8,29 @@ require 'haml'
 
 $pokureru 
 configure do 
-	$pokureru = true
-	set :environment, :production
-	#Haml::Template.options[:ugly] = true  
+  $pokureru = true
+  set :environment, :production
+  #Haml::Template.options[:ugly] = true  
 end
 before do
 end
 
 get '/' do
-	haml :index
+  haml :index
 end
 
 get '/api' do
-	content_type :json
-	p $pokureru
-	if $pokureru then 
+  content_type :json
+  p $pokureru
+  if $pokureru then 
 # 木魚を打つ処理
-		$pokureru = false
-		`./poku.py`
-		$pokureru = true
-		{sound:"poku",info:"日本の心、和の心。"}.to_json
-	else 
-		{sound:"not_poku",info:"叩けませんでした。"}.to_json
-	end 
+    $pokureru = false
+    `./poku.py`
+    $pokureru = true
+    {sound:"poku",info:"日本の心、和の心。"}.to_json
+  else 
+    {sound:"not_poku",info:"叩けませんでした。"}.to_json
+  end 
 end
 
 
